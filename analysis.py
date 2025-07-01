8bd386dcff57b411a1f6e5ed04f5d0d66f401360
Shayan Rahimi <shayanrahimi1378@gmail.com>
2025-07-01 10:00:00 +0200

    Add cycle life estimation

--- analysis.py
+++ analysis.py
@@ -9,3 +9,14 @@ def degradation_rate(capacities, times):
         return 0.0
     coeff = np.polyfit(times, capacities, 1)
     return coeff[0]
+
+import numpy as np
+
+def estimate_eol(capacities, times, eol_threshold=0.8):
+    rate = np.polyfit(times, capacities, 1)
+    if rate[0] >= 0:
+        return None
+    return (eol_threshold - rate[1]) / rate[0]
+
+def capacity_fade(initial, current):
+    return ((initial - current) / initial) * 100
