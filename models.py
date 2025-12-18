534a2c880d2b0e1149d94d0120cdf2919e0d24f5
Shayan Rahimi <shayanrahimi1378@gmail.com>
2025-12-18 10:00:00 +0100

    Implement linear degradation model

--- models.py
+++ models.py
@@ -50,3 +50,18 @@ class EnsembleModel:
     def predict(self, t):
         preds = [m.predict(t) for m in self.models]
         return np.average(preds, weights=self.weights)
+
+import numpy as np
+
+class LinearModel:
+    def __init__(self):
+        self.slope = None
+        self.intercept = None
+
+    def fit(self, times, capacities):
+        coeff = np.polyfit(times, capacities, 1)
+        self.slope, self.intercept = coeff
+        return self
+
+    def predict(self, t):
+        return self.slope * t + self.intercept
