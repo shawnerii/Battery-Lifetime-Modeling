628090a39883397ea98fcbbd507db2c4d50be3b3
Shayan Rahimi <shayanrahimi1378@gmail.com>
2026-01-15 10:00:00 +0100

    Add batch processing for multiple batteries

--- batch.py
+++ batch.py
@@ -20,3 +20,14 @@ def parallel_batch(battery_dict, func, max_workers=4):
     with ThreadPoolExecutor(max_workers=max_workers) as ex:
         results = dict(ex.map(process, keys))
     return results
+
+import numpy as np
+
+def process_batch(battery_dict, func, **kwargs):
+    results = {}
+    for key, data in battery_dict.items():
+        try:
+            results[key] = func(data, **kwargs)
+        except Exception as e:
+            results[key] = None
+    return results
